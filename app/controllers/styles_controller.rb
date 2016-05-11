b588ee532bd3ce14fc17775914005c15196ad62f
Prateek Obireddy <Prateek@192-168-1-5.tpgi.com.au>
2016-05-11 19:53:57 +1000

    Styles and Ingredients create/new and new.html.erb forms created

---
+++ app/controllers/styles_controller.rb
@@ -0,0 +1,24 @@
+
+  class StylesController < ApplicationController
+
+    def new
+      @style = Style.new
+    end
+
+    def create
+      @style = Style.new(style_params)
+      if @style.save
+        flash[:success] = 'Base was created successfully'
+        redirect_to recipes_path
+      else
+        render 'new'
+    end
+  end
+
+    private
+
+    def style_params
+      params.require(:style).permit(:name)
+    end
+
+  end
