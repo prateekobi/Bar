fccec150c2d5a99c0c5d14a92d3b6e7eb2ddc01e
Prateek Obireddy <Prateek@Prateeks-MacBook-Air.local>
2016-05-13 13:29:18 +1000

    Final scene

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,7 +7,7 @@ class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
   validates :bar_id, presence: true
-  validates :name, presence: true, length: {minimum: 5, maximum: 100}
+  validates :name, presence: true, length: {minimum: 3, maximum: 100}
   validates :summary, presence: true, length: {minimum: 10, maximum: 150}
   validates :description, presence: true, length: {minimum: 20, maximum: 500}
   mount_uploader :picture, PictureUploader
