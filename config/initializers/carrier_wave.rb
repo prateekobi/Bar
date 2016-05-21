8a0dd2e31eedcad706e656e8f212be658fe3c229
Prateek Obireddy <Prateek@Prateeks-Air.gateway>
2016-05-21 22:54:36 +1000

    Added region to carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,6 +5,7 @@
         :provider => 'AWS',
         :aws_access_key_id => ENV['S3_ACCESS_KEY'],
         :aws_secret_access_key => ENV['S3_SECRET_KEY']
+        :region => 'ap-southeast-2'
       }
       config.fog_directory = ENV['S3_BUCKET']
     end
